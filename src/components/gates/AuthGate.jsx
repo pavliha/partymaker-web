2f6ec4f2d28419edc2211f0946bcce9152bd7d6b
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-10-01 18:36:38 +0300

    fix shapes

--- src/components/gates/AuthGate.jsx
+++ src/components/gates/AuthGate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { shape, string, any } from 'prop-types'
 import { Redirect, Route } from 'react-router-dom'
 import { connect, select } from 'src/redux'
-import { userShape } from 'shapes/user'
+import { userShape } from 'shapes'
 import Storage from 'services/Storage'
 
 const AuthGate = ({ redux: { user }, location, ...props }) => {
