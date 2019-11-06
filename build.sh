0a73a8ccd55adf296785c7b0eeb66b2a38348fe7
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-11-06 20:32:20 +0200

    update ci config

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 git reset --hard origin/master
 git pull origin master
+sudo service mysql stop
+echo 'Stop mysql service'
 yarn install
 yarn build
+sudo service mysql start
+echo 'Start mysql service'
