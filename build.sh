b68a5358e41d5e40f0197b921ec7e1d4d205c145
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2019-11-17 02:13:33 +0200

    add auto deploy for nginx config

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@ git reset --hard origin/master
 git pull origin master
 sudo service mysql stop
 echo 'Stop mysql service'
+sudo service nginx stop
+echo 'Stop nginx service'
 yarn install
 yarn build
-sudo service mysql start
 echo 'Start mysql service'
+sudo service mysql start
+sudo cp ./config.nginx /etc/nginx/sites-avaliable/partymaker.zp.ua
+echo 'Start nginx service'
+sudo service nginx start
