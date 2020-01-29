290fdebbccf0a3dc20f73b076598d73bed72e20d
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2020-01-29 15:47:40 +0200

    add WYSWG read only editor

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -1,19 +1,23 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 import { object, string } from 'prop-types'
 import { withStyles } from '@material-ui/styles'
 import { Typography } from '@material-ui/core'
 import { placeShape } from 'shapes'
 import isEmpty from 'lodash/isEmpty'
+import { convertFromRaw, Editor, EditorState } from 'draft-js'
+import { stateFromHTML } from 'draft-js-import-html'
+import useTheme from '@material-ui/core/styles/useTheme'
+
 import {
-  PhotosList,
+  AdditionalServicesList,
   Contacts,
+  PhotosList,
+  PlaceActions,
+  PlaceHeader,
   PlaceStatus,
   PlaceSubtitle,
-  PlaceHeader,
-  PricesList,
-  AdditionalServicesList,
-  PlaceActions
+  PricesList
 } from 'components'
 
 const styles = {
@@ -54,61 +58,69 @@ const styles = {
   },
 }
 
-class Place extends PureComponent {
+const getDescription = (description) => {
+  try {
+    return EditorState.createWithContent(convertFromRaw(JSON.parse(description)))
+  } catch (e) {
+    return EditorState.createWithContent(stateFromHTML(description))
+  }
+}
 
-  componentDidMount() {
-    window.scrollTo({ top: 0 })
+const Place = ({ classes, className, place }) => {
+  const theme = useTheme()
+
+  const styleMap = {
+    'TITLE': theme.typography.h5,
+    'SUBTITLE': theme.typography.subtitle2,
+    'BODY': theme.typography.body1,
   }
 
-  render() {
-    const { classes, className, place } = this.props
-
-    return (
-      <section className={classNames(classes.root, className)}>
-        <PlaceHeader place={place} />
-        {place.requirements && <PlaceStatus className={classes.status} requirements={place.requirements} />}
-        <section className={classes.container}>
-          <div className={classes.actions}>
-            <PlaceActions
-              phone={place.contacts?.phone}
-              website_url={place.contacts?.website_url}
+  return (
+    <section className={classNames(classes.root, className)}>
+      <PlaceHeader place={place} />
+      {place.requirements && <PlaceStatus className={classes.status} requirements={place.requirements} />}
+      <section className={classes.container}>
+        <div className={classes.actions}>
+          <PlaceActions
+            phone={place.contacts?.phone}
+            website_url={place.contacts?.website_url}
+          />
+        </div>
+        <PhotosList
+          className={classes.photosList}
+          photos={place.photos}
+        />
+        {isEmpty(place.prices) || (
+          <div className={classes.section}>
+            <PlaceSubtitle>Цены</PlaceSubtitle>
+            <PricesList prices={place.prices} />
+            <Typography color="textSecondary">{place.about_prices}</Typography>
+          </div>
+        )}
+        {isEmpty(place.additional_services) || (
+          <div className={classes.section}>
+            <PlaceSubtitle>Дополнительные услуги</PlaceSubtitle>
+            <AdditionalServicesList additional_services={place.additional_services} />
+          </div>
+        )}
+        {isEmpty(place.contacts) || (
+          <div className={classes.section}>
+            <PlaceSubtitle>Контакты</PlaceSubtitle>
+            <Contacts contacts={place.contacts} />
+          </div>
+        )}
+        {place.description && (
+          <div className={classes.description}>
+            <Editor
+              customStyleMap={styleMap}
+              editorState={getDescription(place.description)}
+              readOnly
             />
           </div>
-          <PhotosList
-            className={classes.photosList}
-            photos={place.photos}
-          />
-          {isEmpty(place.prices) || (
-            <div className={classes.section}>
-              <PlaceSubtitle>Цены</PlaceSubtitle>
-              <PricesList prices={place.prices} />
-              <Typography color="textSecondary">{place.about_prices}</Typography>
-            </div>
-          )}
-          {isEmpty(place.additional_services) || (
-            <div className={classes.section}>
-              <PlaceSubtitle>Дополнительные услуги</PlaceSubtitle>
-              <AdditionalServicesList additional_services={place.additional_services} />
-            </div>
-          )}
-          {isEmpty(place.contacts) || (
-            <div className={classes.section}>
-              <PlaceSubtitle>Контакты</PlaceSubtitle>
-              <Contacts contacts={place.contacts} />
-            </div>
-          )}
-          {place.description && (
-            <div className={classes.description}>
-              <Typography
-                className={classes.descriptionText}
-                dangerouslySetInnerHTML={{ __html: place.description }}
-              />
-            </div>
-          )}
-        </section>
+        )}
       </section>
-    )
-  }
+    </section>
+  )
 }
 
 Place.propTypes = {
