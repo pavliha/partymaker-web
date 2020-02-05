4731e04d64f7d342c9723665cbfcd839ec913f78
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2020-02-05 23:44:26 +0200

    change deployment sequence

--- build.sh
+++ build.sh
@@ -4,21 +4,12 @@ echo 'Fetching new changes'
 git reset --hard origin/master
 git pull origin master
 
-echo 'Stop mysql service'
-sudo service mysql stop
-
-sudo service nginx stop
-echo 'Stop nginx service'
-
 echo 'Start project'
 yarn install
 /home/dev/.npm-global/bin/pm2 restart index.js --name web -- --stage=prod
-
-echo 'Start mysql service'
-sudo service mysql start
 
 echo 'Copy new nginx config'
 sudo cp ./config.nginx /etc/nginx/sites-available/web.partymaker.zp.ua
 
 echo 'Start nginx service'
-sudo service nginx start
+sudo service nginx reload
