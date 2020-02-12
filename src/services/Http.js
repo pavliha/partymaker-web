b729e30e7ad15760c9c16a91601abba0075b09c0
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2020-02-12 13:15:18 +0200

    replace Http.js with newer one

--- src/services/Http.js
+++ src/services/Http.js
@@ -1,66 +1,25 @@
 import axios from 'axios'
 import { store, actions } from 'src/redux'
-import { BACKEND_URL } from 'src/config/app'
 
-class Http {
-  constructor() {
-    this.instance = axios.create({
-      baseURL: BACKEND_URL,
-      timeout: 20000,
-    })
-  }
+const http = axios.create({
+  baseURL: process.env.BACKEND_URL,
+  timeout: 20000,
+})
 
-  logout() {
-    store.dispatch(actions.auth.logout())
-  }
+http.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${store.getState().auth?.token}`
+  return config
+})
 
-  authorize() {
-    const token = store.getState().auth?.token
-    const { headers } = this.instance.defaults
-    if (!token) return
-    headers.common.Authorization = `Bearer ${token}`
-  }
-
-  handleError(err) {
-    if (err?.response?.status === 401) this.logout()
-    if (err?.response?.status === 404) throw err
-    if (err?.response?.data) throw err.response.data
-    if (err?.response) throw err.response
-    if (err) throw err
-  }
-
-  async request(method, url, params, config) {
-    this.authorize()
-    try {
-      const { data } = await this.instance[method](url, params, config)
-
-      return data
-    } catch (err) {
-      this.handleError(err)
-    }
-  }
-
-  get(url, params) {
-    return this.request('get', url, params)
-  }
-
-  post(url, params, config) {
-    return this.request('post', url, params, config)
-  }
-
-  put(url, params) {
-    return this.request('put', url, params)
-  }
-
-  delete(url, params) {
-    return this.request('delete', url, params)
-  }
-
-  patch(url, params) {
-    return this.request('patch', url, params)
-  }
-}
-
-const http = new Http()
+// Add a response interceptor
+http.interceptors.response.use(
+  response => response.data,
+  errors => {
+    if (errors?.response?.status === 401) store.dispatch(actions.auth.logout())
+    if (errors?.response?.status === 404) throw errors
+    if (errors?.response?.data) throw errors.response.data
+    if (errors?.response) throw errors.response
+    if (errors) throw errors
+  })
 
 export default http
